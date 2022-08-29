c693700c3aa6939d1eab3c1ecaace1e9e073f6c2
nrcool <nomanalam7214@gmail.com>
2022-08-29 14:11:42 +0200

    useSate and useEffect hook, fetch data from external api

--- SPA-React-LiveCoding/React-E-Commerce-App/src/pages/Product.js
+++ SPA-React-LiveCoding/React-E-Commerce-App/src/pages/Product.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Product() {
+  const location = useLocation()
+  console.log(location)
   return (
-    <div><h1>Single Product Page</h1></div>
+    <div><h1>{location.state.title}</h1>
+      <img src={location.state.image} width="200" alt="" />
+      <h3>{location.state.description}</h3>
+      
+    </div>
   )
 }
--- SPA-React-LiveCoding/React-E-Commerce-App/src/pages/Products.js
+++ SPA-React-LiveCoding/React-E-Commerce-App/src/pages/Products.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component"
+
 
 export default function Products() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+      });
+  }, []);
+
+  console.log("products page render ......");
+
   return (
-    <div><h1>Projects Page</h1></div>
-  )
+    <div>
+      <h1>Projects Page</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {data.map((product) => {
+          return (
+            <div
+              style={{
+                width: "300px",
+                boxShadow: "2px 1px 5px gray",
+                borderRadius:"20px",
+                margin: "10px",
+                display:"flex",
+                alignItems:"center",
+                flexDirection:"column"
+              }}
+            >
+              <Link to={`/products/${product.id}`} state={product}>
+                <img src={product.image} width="200" height="200" alt="" />{" "}
+              </Link>
+              <h3>{product.title}</h3>
+              <h3>$ {product.price}</h3>
+               <ReactStars
+               count={5}
+               value={product.rating.rate}
+               activeColor="yellow"
+               isHalf={true}
+               size={24}
+               edit={false}
+               /> 
+              <button>add to cart</button>
+
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
